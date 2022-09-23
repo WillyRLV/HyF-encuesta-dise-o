2e8352640504d228967bee0d9cf3465fa5f513ed
William <Williamlv1998@gmail.com>
2022-09-23 09:59:58 -0500

    avance

---
+++ app.js
@@ -0,0 +1,38 @@
+var form = document.forms['myform']
+
+form.onsubmit = function(e){
+    
+    e.preventDefault();
+    // if (e.target.value == '1111') {
+    //     window.location.replace("/form.html")
+
+    // }
+    // else {
+    //     e.preventDefault();
+    //             alert("no existe esa identificacion")
+    //         }
+var dni = document.getElementById('DNI').value;
+    console.log(dni);
+
+    if (dni == '1111') {
+                window.location.replace("/form.html")
+        
+            }
+            else {
+                alert("no existe esa identificacion")
+            }
+}
+
+// function auth() {
+
+//     const un = document.forms["myform"]["DNI"].value
+
+//     if (un == '1111') {
+//         window.location.replace("/form.html")
+
+//     }
+//     else {
+//         alert("no existe esa identificacion")
+//     }
+
+// }
